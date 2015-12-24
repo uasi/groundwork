c93295df64067e5afd1837e6197cc09da324a44b
Tomoki Aonuma <uasi@uasi.jp>
2015-12-25 00:11:29 +0900

    Add wildcard A record to opts-io.tf

--- opts-io.tf
+++ opts-io.tf
@@ -12,3 +12,11 @@ resource "aws_route53_record" "opts-io_a" {
     ttl = "300"
     records = ["188.166.208.27"]
 }
+
+resource "aws_route53_record" "opts-io_awild" {
+    zone_id = "${aws_route53_zone.opts-io.zone_id}"
+    name = "*"
+    type = "A"
+    ttl = "300"
+    records = ["188.166.208.27"]
+}
