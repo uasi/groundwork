7d95be6ffe005a47eb14edba8dd488414373e58d
Tomoki Aonuma <uasi@uasi.jp>
2015-12-24 22:21:01 +0900

    Add aws_route53_record resource to opts-io.tf

--- opts-io.tf
+++ opts-io.tf
@@ -4,3 +4,11 @@ resource "aws_route53_zone" "opts-io" {
         prevent_destroy = true
     }
 }
+
+resource "aws_route53_record" "opts-io_a" {
+    zone_id = "${aws_route53_zone.opts-io.zone_id}"
+    name = "opts.io"
+    type = "A"
+    ttl = "300"
+    records = ["188.166.208.27"]
+}
