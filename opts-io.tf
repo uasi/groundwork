1f2b94a5f4a4f293689f0154f4ed206e5a46e8c2
Tomoki Aonuma <uasi@uasi.jp>
2016-02-20 22:46:09 +0900

    Add keybase verification TXT to opts.io

--- opts-io.tf
+++ opts-io.tf
@@ -20,3 +20,13 @@ resource "aws_route53_record" "opts-io_awild" {
     ttl = "300"
     records = ["133.130.102.137"]
 }
+
+resource "aws_route53_record" "opts-io_txt" {
+    zone_id = "${aws_route53_zone.opts-io.zone_id}"
+    name = "opts.io"
+    type = "TXT"
+    ttl = "300"
+    records = [
+      "keybase-site-verification=tCZRLGFWxKvAWWs6IXiCr2ShB5hq9wW12colDD40gos"
+    ]
+}
