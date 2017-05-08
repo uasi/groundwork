d8713c89acd4addeb4647f230fed781fe8ab4a6a
Tomoki Aonuma <uasi@uasi.jp>
2017-05-08 19:23:45 +0900

    Remove AWS provider and related variables

--- providers.tf
+++ providers.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "${var.aws_access_key_id}"
-  secret_key = "${var.aws_secret_access_key}"
-  region     = "${var.aws_region}"
-}
-
 provider "cloudflare" {
   email = "${var.cloudflare_email}"
   token = "${var.cloudflare_token}"
