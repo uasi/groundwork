473d45930908f64966612479d12b994f5bcc06ae
Tomoki Aonuma <uasi@uasi.jp>
2019-03-18 23:33:44 +0900

    Add records for opts.io

--- opts-io.tf
+++ opts-io.tf
@@ -1,3 +1,19 @@
+resource "cloudflare_record" "opts-io_a" {
+  domain  = "opts.io"
+  type    = "A"
+  name    = "@"
+  value   = "149.28.22.95"
+  proxied = true
+}
+
+resource "cloudflare_record" "opts-io_cname_hnr-rec" {
+  domain  = "opts.io"
+  type    = "CNAME"
+  name    = "hnr-rec"
+  value   = "opts.io"
+  proxied = true
+}
+
 resource "cloudflare_record" "opts-io_txt" {
   domain = "opts.io"
   type   = "TXT"
