0b2e394890c0b79259a9adf05823d687844b1c76
Tomoki Aonuma <uasi@uasi.jp>
2020-08-16 23:33:58 +0900

    Upgrade to Terraform 0.13

--- providers.tf
+++ providers.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-  version = "~> 2.0"
   region  = "ap-northeast-1"
   profile = "terraform-groundwork"
 }
 
 provider "cloudflare" {
-  version    = "~> 2.0"
   email      = var.cloudflare_email
   api_key    = var.cloudflare_api_key
   account_id = "f60414dd0930fcfea6a07ebb25b1e58f"
---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
