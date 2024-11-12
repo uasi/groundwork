a4cf56940a3d6d9d33789a86657eb379975d957e
Tomoki Aonuma <uasi@uasi.jp>
2024-11-12 14:45:56 +0900

    Upgrade providers

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
   required_version = "~> 1.0"
